6dd48085ecdedcac9cca5fc8bdcfa449f9f288c4
mindprison <p.aboalfathzade@gmail.com>
2023-09-22 01:12:11 -0700

    day2-2

--- Catalogue/src/App.jsx
+++ Catalogue/src/App.jsx
@@ -1,9 +1,16 @@
-
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
+const learn = {
+  learner:"I'm learning",
+  title:"React"
+}
 
 function App() {
   return (
     <div>
-      <h1>Im learning React</h1>
+      <h1>{learn.learner} {learn.title}</h1>
+      <label htmlFor="search" >Search Here</label><br/>
+      <input type="text" id="search" placeholder="it doesn't work yet"/>
     </div>
   )
 }
